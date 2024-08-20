1524a1a89f893f324486c936684a109ffd633521
Treasure56 <simplicitytreasure@gmail.com>
2024-08-20 14:02:48 +0100

    setting up stripes

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -19,14 +19,14 @@ export async function POST(req: Request) {
       process.env.STRIPE_WEBHOOK_SECRET!
     );
 
-    console.log(event);
     
     if (event.type === "checkout.session.completed") {
       if (!event.data.object.customer_details?.email) {
         throw new Error("Missing user email");
       }
-
+      
       const session = event.data.object as Stripe.Checkout.Session;
+      console.log(session);
 
       const { userId, orderId } = session.metadata || {
         userId: null,
